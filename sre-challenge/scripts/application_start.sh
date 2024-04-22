715de160a604b4935043c4b09d518cef2c3919ef
Melvin Kimathi <karutikimathi@MKIMATHI-ENG.local>
2024-04-22 15:41:39 +0300

    restart nginx to pick up uWSGI configs

--- sre-challenge/scripts/application_start.sh
+++ sre-challenge/scripts/application_start.sh
@@ -5,4 +5,4 @@ cd /var/www/html/django_project
 sudo /var/www/html/django_project/.venv/bin/uwsgi --socket /var/run/uwsgi/app/socket --module sre_challenge.wsgi:application --daemonize /var/log/uwsgi/sre-challenge.log
 
 # restart reverse proxy
-sudo systemctl restart nginx
+sudo systemctl restart nginx.service
